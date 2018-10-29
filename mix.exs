f0dfc36def6fb83c58332f936146741e2c494192
David Whitlock <alovedalongthe@gmail.com>
2018-10-29 12:46:23 +0700

    Fixes #87

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Phauxth.Mixfile do
   use Mix.Project
 
-  @version "1.2.7"
+  @version "1.2.8"
 
   @description """
   Authentication library for Phoenix, and other Plug-based, web applications
